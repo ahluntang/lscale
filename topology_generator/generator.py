15c6aa7f49d0340160ba4ffb6df2e6fa7825ffb6
Ah-Lun Tang <tang@ahlun.be>
2013-02-25 11:23:07 +0100

    added base for topology generator script

---
+++ topology_generator/generator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os, traceback, logging
+
+from lscale_pexpect_utils import Bridge, Container, VirtualLink
+##########
+## Main ##
+##########
+def main():
+	    return 0
+
+if __name__ == "__main__":
+    try:
+        main()
+    except SystemExit, e:
+        raise e
+    except Exception, e:
+        print "ERROR"
+        print str(e)
+        logger = logging.getLogger(__name__)
+        logger.exception(str(e))
+        traceback.print_exc()
+        os._exit(1) 
