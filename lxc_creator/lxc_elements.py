86abc0a5de65b259b504d2d5e7ae72ec932be2cb
Ah-Lun Tang <tang@ahlun.be>
2013-03-05 10:52:30 +0100

    routes list
    Signed-off-by: Ah-Lun Tang <tang@ahlun.be>

--- lxc_creator/lxc_elements.py
+++ lxc_creator/lxc_elements.py
@@ -72,7 +72,8 @@ class Container( object ):
         self.routingscript     = None
         self.postroutingscript = None
         self.prerouting        = { 'container_id' : self.container_id }
-        self.routing           = { 'container_id' : self.container_id, 'routes': list }
+        self.routing           = { 'container_id' : self.container_id }
+        self.routing['routes'] = []
         self.postrouting       = { 'container_id' : self.container_id }
         self.interfaces        = 0
 
