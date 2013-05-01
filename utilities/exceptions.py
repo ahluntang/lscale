1d436dfd00489991cc6b0dceffc17685a5e1c61a
Ah-Lun Tang <tang@ahlun.be>
2013-05-01 19:39:12 +0200

    refactor exceptions

--- utilities/exceptions.py
+++ utilities/exceptions.py
@@ -1,48 +1,47 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-class TopologyException(Exception) :
-    def __init__(self, message, Errors) :
+class LargeScaleException(Exception) :
+    def __init__(self, message, Errors = None) :
         Exception.__init__(self, message)
 
         self.Errors = Errors
 
-
-class InterfaceNotFoundException(Exception) :
-    def __init__(self, message, Errors) :
-        TopologyException.__init__(self, message, Errors)
-
-
-class LinkNotFoundException(Exception) :
-    def __init__(self, message, Errors) :
-        TopologyException.__init__(self, message, Errors)
+class InterfaceNotFoundException(LargeScaleException) :
+    def __init__(self, message, Errors = None) :
+        LargeScaleException.__init__(self, message, Errors)
 
 
-class ContainerNotFoundException(Exception) :
-    def __init__(self, message, Errors) :
-        TopologyException.__init__(self, message, Errors)
+class LinkNotFoundException(LargeScaleException) :
+    def __init__(self, message, Errors = None) :
+        LargeScaleException.__init__(self, message, Errors)
 
 
-class BridgeNotFoundException(Exception) :
-    def __init__(self, message, Errors) :
-        TopologyException.__init__(self, message, Errors)
+class ContainerNotFoundException(LargeScaleException) :
+    def __init__(self, message, Errors = None) :
+        LargeScaleException.__init__(self, message, Errors)
 
 
-class ComponentException(Exception) :
-    def __init__(self, message, Errors) :
-        TopologyException.__init__(self, message, Errors)
+class BridgeNotFoundException(LargeScaleException) :
+    def __init__(self, message, Errors = None) :
+        LargeScaleException.__init__(self, message, Errors)
 
 
-class NoFreeInterfacesException(Exception) :
-    def __init__(self, message, Errors) :
+class ComponentException(LargeScaleException) :
+    def __init__(self, message, Errors = None) :
+        LargeScaleException.__init__(self, message, Errors)
+
+
+class NoFreeInterfacesException(ComponentException) :
+    def __init__(self, message, Errors = None) :
         ComponentException.__init__(self, message, Errors)
 
 
-class IPComponentException(Exception) :
-    def __init__(self, message, Errors) :
-        TopologyException.__init__(self, message, Errors)
+class IPComponentException(LargeScaleException) :
+    def __init__(self, message, Errors = None) :
+        LargeScaleException.__init__(self, message, Errors)
 
 
-class InsufficientRightsException(Exception) :
-    def __init__(self, message, Errors) :
-        TopologyException.__init__(self, message, Errors)
+class InsufficientRightsException(LargeScaleException) :
+    def __init__(self, message, Errors = None) :
+        LargeScaleException.__init__(self, message, Errors)
