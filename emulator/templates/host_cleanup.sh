9ebead48645e4c3b08b6b18ce7e59a1ce7aede0c
Ah-Lun Tang <tang@ahlun.be>
2013-05-02 17:56:07 +0200

    empty template for host cleanup

--- emulator/templates/host_cleanup.sh
+++ emulator/templates/host_cleanup.sh
@@ -2,20 +2,20 @@
 # -*- coding: utf-8 -*-
 
 
-ssh_pids=(`pidof sshd | tr " " "\n"`)
+#ssh_pids=(`pidof sshd | tr " " "\n"`)
 
 
-main_ssh=`cat /var/run/sshd.pid`
+#main_ssh=`cat /var/run/sshd.pid`
 
-echo $main_ssh
+#echo $main_ssh
 
-for pid in "${ssh_pids[@]}"
-do
-	if [ "$pid" == "$main_ssh" ]
-	then
-	    echo Not killing main ssh: $pid
-	else
-	    echo Killing ssh with pid $pid
-	    sudo kill $pid
-	fi
-done
+#for pid in "${ssh_pids[@]}"
+#do
+#	if [ "$pid" == "$main_ssh" ]
+#	then
+#	    echo Not killing main ssh: $pid
+#	else
+#	    echo Killing ssh with pid $pid
+#	    sudo kill $pid
+#	fi
+#done
