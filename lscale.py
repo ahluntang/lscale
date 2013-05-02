52fb9a147507a7a5e7e7e9ee69a0f4459ee0ec97
Ah-Lun Tang <tang@ahlun.be>
2013-05-02 10:47:20 +0200

    updated lscale.py with python3 support

--- lscale.py
+++ lscale.py
@@ -11,20 +11,23 @@ import logging
 import generator
 import emulator
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Large-Scale Framework.')
-    subparsers = parser.add_subparsers(help='sub-command help',dest='subparser_name')
+    subparsers = parser.add_subparsers(help='sub-command help', dest='subparser_name')
 
     #parser for generating topologies
     genparser = subparsers.add_parser('generate', help='help for generating topologies')
-    genparser.add_argument('-f', '--file', default = 'output/topology.xml', help = 'output file to write to.', required = False)
-    genparser.add_argument('-e', '--example', default = 'smalltop', help = 'example to create topology for', required = False)
+    genparser.add_argument('-f', '--file', default='output/topology.xml', help='output file to write to.',
+                           required=False)
+    genparser.add_argument('-e', '--example', default='smalltop', help='example to create topology for', required=False)
 
 
     # parser for emulating topologies
     emparser = subparsers.add_parser('emulate', help='help for emulating topologies')
-    emparser.add_argument('-f', '--file', default = 'output/topology.xml', help = 'input file.', required = False)
-    emparser.add_argument('-i', '--id', default = 'h001', help = 'host id that should be used to parse and create containers for', required = False)
+    emparser.add_argument('-f', '--file', default='output/topology.xml', help='input file.', required=False)
+    emparser.add_argument('-i', '--id', default='h001',
+                          help='host id that should be used to parse and create containers for', required=False)
 
     return vars(parser.parse_args())
 
@@ -33,38 +36,42 @@ def parse_arguments():
 ## Main ##
 ##########
 def main():
-
     # set logging
     try:
         logger.set_logging(logging.DEBUG)
-    except Exception, e:
-        print "Could not configure logging framework."
-        raise e
+    except:
+        err_msg = "Could not configure logging framework."
+        print(err_msg)
+        raise exceptions.LoggingException(err_msg)
 
     # parse arguments
     try:
         args = parse_arguments()
-    except Exception, e:
-        logging.getLogger(__name__).exception("Could not parse arguments.")
-        raise e
-
+    except:
+        err_msg = "Could not configure logging framework."
+        logging.getLogger(__name__).exception(err_msg)
+        raise exceptions.ArgParseException(err_msg)
 
     # run generator or emulator based on arguments
-    if args['subparser_name'] == "generate" :
+    if args['subparser_name'] == "generate":
         try:
-            generator.generate(args['example'],args['file'])
-        except Exception, e :
-            logging.getLogger(__name__).exception("Could not generate topology.")
+            generator.generate(args['example'], args['file'])
+        except exceptions.GeneratorException as e:
+            err_msg = "Could not generate topology."
+            logging.getLogger(__name__).exception(err_msg)
+            raise e
 
-    elif args['subparser_name'] == "emulate" :
+    elif args['subparser_name'] == "emulate":
         parsed_topology = {}
         try:
             emulator.emulate(args['file'], args['id'], parsed_topology)
-        except exceptions.InsufficientRightsException :
-            logging.getLogger(__name__).exception("Could not emulate topology.")
+        except exceptions.InsufficientRightsException as e:
+            err_msg = "Could not emulate topology."
+            logging.getLogger(__name__).exception(err_msg)
             os._exit(1)
-        except Exception, e:
-            logging.getLogger(__name__).exception("Could not emulate topology.")
+        except exceptions.GeneratorException as e:
+            err_msg = "Could not emulate topology."
+            logging.getLogger(__name__).exception(err_msg)
             raise e
     else:
         raise exceptions.IncorrectArgumentsException("Error: check your arguments.")
@@ -75,14 +82,8 @@ def main():
 
 if __name__ == "__main__":
     try:
-
         main()
-
-    except SystemExit, e:
-        raise e
-        os._exit(1)
-
-    except Exception, e:
+    except Exception as e:
         logger = logging.getLogger(__name__)
         logger.exception(str(e))
         logger.exception(traceback.print_exc())
--- utilities/exceptions.py
+++ utilities/exceptions.py
@@ -1,52 +1,74 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-class LargeScaleException(Exception) :
-    def __init__(self, message, Errors = None) :
+
+class LargeScaleException(Exception):
+    def __init__(self, message, Errors=None):
         Exception.__init__(self, message)
 
         self.Errors = Errors
 
-class InterfaceNotFoundException(LargeScaleException) :
-    def __init__(self, message, Errors = None) :
+
+class LoggingException(LargeScaleException):
+    def __init__(self, message, Errors=None):
         LargeScaleException.__init__(self, message, Errors)
 
 
-class LinkNotFoundException(LargeScaleException) :
-    def __init__(self, message, Errors = None) :
+class ArgParseException(LargeScaleException):
+    def __init__(self, message, Errors=None):
         LargeScaleException.__init__(self, message, Errors)
 
 
-class ContainerNotFoundException(LargeScaleException) :
-    def __init__(self, message, Errors = None) :
+class GeneratorException(LargeScaleException):
+    def __init__(self, message, Errors=None):
         LargeScaleException.__init__(self, message, Errors)
 
 
-class BridgeNotFoundException(LargeScaleException) :
-    def __init__(self, message, Errors = None) :
+class EmulatorException(LargeScaleException):
+    def __init__(self, message, Errors=None):
         LargeScaleException.__init__(self, message, Errors)
 
 
-class ComponentException(LargeScaleException) :
-    def __init__(self, message, Errors = None) :
+class InterfaceNotFoundException(LargeScaleException):
+    def __init__(self, message, Errors=None):
         LargeScaleException.__init__(self, message, Errors)
 
 
-class NoFreeInterfacesException(ComponentException) :
-    def __init__(self, message, Errors = None) :
+class LinkNotFoundException(LargeScaleException):
+    def __init__(self, message, Errors=None):
+        LargeScaleException.__init__(self, message, Errors)
+
+
+class ContainerNotFoundException(LargeScaleException):
+    def __init__(self, message, Errors=None):
+        LargeScaleException.__init__(self, message, Errors)
+
+
+class BridgeNotFoundException(LargeScaleException):
+    def __init__(self, message, Errors=None):
+        LargeScaleException.__init__(self, message, Errors)
+
+
+class ComponentException(LargeScaleException):
+    def __init__(self, message, Errors=None):
+        LargeScaleException.__init__(self, message, Errors)
+
+
+class NoFreeInterfacesException(ComponentException):
+    def __init__(self, message, Errors=None):
         ComponentException.__init__(self, message, Errors)
 
 
-class IPComponentException(LargeScaleException) :
-    def __init__(self, message, Errors = None) :
+class IPComponentException(LargeScaleException):
+    def __init__(self, message, Errors=None):
         LargeScaleException.__init__(self, message, Errors)
 
 
-class InsufficientRightsException(LargeScaleException) :
-    def __init__(self, message, Errors = None) :
+class InsufficientRightsException(LargeScaleException):
+    def __init__(self, message, Errors=None):
         LargeScaleException.__init__(self, message, Errors)
 
 
-class IncorrectArgumentsException(LargeScaleException) :
-    def __init__(self, message, Errors = None) :
-        LargeScaleException.__init__(self, message, Errors)
+class IncorrectArgumentsException(EmulatorException):
+    def __init__(self, message, Errors=None):
+        EmulatorException.__init__(self, message, Errors)
