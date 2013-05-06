af7ed9fb8734bbf135cc0e7cd3dd6bade325af2f
Ah-Lun Tang <tang@ahlun.be>
2013-05-06 21:15:56 +0200

    dynamic loading modules & eval

--- generator/__init__.py
+++ generator/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 
 from generator.topology import elements, exporter
 
@@ -8,18 +9,7 @@ from generator.examples import *
 
 
 def generate(example, filename, starting_address="172.16.0.0", last_host_id=0, last_container_id=0, last_link_id=0):
-    if example == 'cityflow':
-        # create an example topology: cityflow preaggregation phase.
-        # see examples package for more info
-        created_topology = cityflow.create(last_host_id, last_container_id, last_link_id, starting_address)
-    elif example == 'citybus':
-        created_topology = citybus.create(last_host_id, last_container_id, last_link_id, starting_address)
-    elif example == 'citystar':
-        created_topology = citystar.create(last_host_id, last_container_id, last_link_id, starting_address)
-    elif example == 'smalltop':
-        created_topology = smalltop.create(last_host_id, last_container_id, last_link_id, starting_address)
-    else:
-        created_topology = cityring.create(last_host_id, last_container_id, last_link_id, starting_address)
-        # export topology to xml file
+    str = "%s.create(last_host_id, last_container_id, last_link_id, starting_address)" % example
+    created_topology = eval(str)
     exporter.write_topology_xml(created_topology, filename)
 
--- generator/examples/__init__.py
+++ generator/examples/__init__.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__all__ = ["citybus", "cityflow", "cityring", "citystar", "smalltop"]
+#__all__ = ["citybus", "cityflow", "cityring", "citystar", "smalltop"]
+
+import os
+import glob
+__all__ = [os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__)+"/*.py")]
+
