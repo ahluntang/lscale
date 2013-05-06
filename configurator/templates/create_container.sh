70c3081e1edb224b4efbb1345d87f5d0c13302e3
Ah-Lun Tang <tang@ahlun.be>
2013-05-06 11:07:30 +0200

    script typo

--- configurator/templates/create_container.sh
+++ configurator/templates/create_container.sh
@@ -6,16 +6,20 @@ BACKING_STORE=$2
 TEMPLATE=$3
 
 
-if [ $# -eq 0 ] ; then
+if [ $# -eq 0 ]
+then
     lxc-create -n base -t ubuntu
     exit 0
-elif [ $# -eq 1 ]; then
+elif [ $# -eq 1 ]
+then
     lxc-create -n ${CONTAINER_NAME}  -t ubuntu
     exit 0
-elif[ $# -eq 2 ]; then
+elif [ $# -eq 2 ]
+then
     lxc-create -n ${CONTAINER_NAME} -B ${BACKING_STORE}  -t ubuntu
     exit 0
-elif[ $# -eq 2 ]; then
+elif [ $# -eq 2 ]
+then
     lxc-create -n ${CONTAINER_NAME} -B ${BACKING_STORE}  -t ${TEMPLATE}
     exit 0
 else
