d2a6a9bc267e281cac255fcf1a5dfcc76809d173
Ah-Lun Tang <tang@ahlun.be>
2013-05-06 10:23:49 +0200

    subprocess output

--- configurator/__init__.py
+++ configurator/__init__.py
@@ -23,6 +23,8 @@ def configure():
 def create_base_container(container_name="base", backing_store=""):
     cmd = "./configurator/create_base.sh %s %s" % (container_name, backing_store)
     subprocess.call(cmd, shell=True)
+    output, error = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    print(output)
 
 
 def clone_container(container_name, original_container="base", snapshot=False):
@@ -30,4 +32,5 @@ def clone_container(container_name, original_container="base", snapshot=False):
         cmd = "./configurator/clone_container.sh %s %s %s" % (container_name, original_container, "snapshot")
     else:
         cmd = "./configurator/clone_container.sh %s %s" % (container_name, original_container)
-    subprocess.call(cmd, shell=True)
+    output, error = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    print(output)
