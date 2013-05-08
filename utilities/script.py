25c33dae730a3a9fefe4a99ae0c9c29c6e3759a9
Ah-Lun Tang <tang@ahlun.be>
2013-05-08 17:50:32 +0200

    fix typo

--- utilities/script.py
+++ utilities/script.py
@@ -46,8 +46,7 @@ def enqueue_output(out, queue):
 
 
 def command(cmd):
-    print(cmd)
-    p = Popen(cmd, stdout=PIPE, stderr=PIPE, bufsize=1, close_fds=ON_POSIX)
+    p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, bufsize=1, close_fds=ON_POSIX)
     q = Queue()
     t = Thread(target=enqueue_output, args=(p.stdout, q))
     t.daemon = True
