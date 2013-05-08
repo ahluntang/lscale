2b5861ceeeb100e39f12be9822770d04686f0163
Ah-Lun Tang <tang@ahlun.be>
2013-05-08 17:10:42 +0200

    multiprocessing test

--- utilities/script.py
+++ utilities/script.py
@@ -5,22 +5,23 @@ import subprocess
 from utilities import exceptions
 
 
-def reader(shell, lines):
-        for line in shell.stdout:
-            lines.append(line)
-            sys.stdout.write(line)
+def reader(cmd, lines):
+    shell = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    for line in shell.stdout:
+        lines.append(line)
+        sys.stdout.write(line)
+    return lines
 
 
 def command(cmd):
-    shell = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
     #output, error = shell.communicate()
     lines = []
 
-    t = multiprocessing.Process(target=reader(shell, lines))
+    t = multiprocessing.Process(target=reader, args=(cmd, lines))
     t.start()
-    shell.wait()
     t.join()
 
-    if shell.returncode != 0:
-        err_msg = "Script error: %s\nOUTPUT\n %s" % (cmd, lines)
-        raise exceptions.ScriptException(err_msg)
+    #if shell.returncode != 0:
+    #    err_msg = "Script error: %s\nOUTPUT\n %s" % (cmd, lines)
+    #    raise exceptions.ScriptException(err_msg)
