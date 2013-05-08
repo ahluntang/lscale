4e9e752032edaffea76558430f3ec1cacc844959
Ah-Lun Tang <tang@ahlun.be>
2013-05-08 17:18:01 +0200

    threading

--- utilities/script.py
+++ utilities/script.py
@@ -1,27 +1,26 @@
 import sys
-import multiprocessing
+import threading
 import subprocess
 
 from utilities import exceptions
 
 
-def reader(cmd, lines):
-    shell = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    for line in shell.stdout:
-        lines.append(line)
-        sys.stdout.write(line)
-    return lines
+def reader(shell, lines):
+        for line in shell.stdout:
+            lines.append(line)
+            sys.stdout.write(line)
 
 
 def command(cmd):
-
+    shell = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     #output, error = shell.communicate()
     lines = []
 
-    t = multiprocessing.Process(target=reader, args=(cmd, lines))
+    t = threading.Thread(target=reader(shell, lines))
     t.start()
+    #shell.wait()
     t.join()
 
-    #if shell.returncode != 0:
-    #    err_msg = "Script error: %s\nOUTPUT\n %s" % (cmd, lines)
-    #    raise exceptions.ScriptException(err_msg)
+    if shell.returncode != 0:
+        err_msg = "Script error: %s\nOUTPUT\n %s" % (cmd, lines)
+        raise exceptions.ScriptException(err_msg)
