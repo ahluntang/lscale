6f68eedcb93bff6318991222e6675cc1840393df
Ah-Lun Tang <tang@ahlun.be>
2013-05-18 19:59:27 +0200

    configs

--- configurator/templates/create_lvm.sh
+++ configurator/templates/create_lvm.sh
@@ -57,10 +57,8 @@ else
     # create new empty cache folder, add mount info to fstab and mount it.
     mkdir /var/cache/lxc
     mkdir /var/lib/lxc
-    MOUNTCACHE="/dev/${VOLUMEGROUP}/cache /var/cache/lxc ext4 errors=remount-ro 0 1"
-    MOUNTLIB="/dev/${VOLUMEGROUP}/lib /var/lib/lxc ext4 errors=remount-ro 0 1"
-    add_line_to_config ${MOUNTCACHE} '/etc/fstab'
-    add_line_to_config ${MOUNTLIB} '/etc/fstab'
+    add_line_to_config "/dev/${VOLUMEGROUP}/cache /var/cache/lxc ext4 errors=remount-ro 0 1" '/etc/fstab'
+    add_line_to_config "/dev/${VOLUMEGROUP}/lib /var/lib/lxc ext4 errors=remount-ro 0 1" '/etc/fstab'
     mount -a /dev/${VOLUMEGROUP}/cache
     mount -a /dev/${VOLUMEGROUP}/lib
 fi
