578e33442792a2d0b5620bae8caf838c44f0c389
Ah-Lun Tang <tang@ahlun.be>
2013-05-20 23:26:33 +0200

    routeflow topology

--- emulator/templates/start_controller.sh
+++ emulator/templates/start_controller.sh
@@ -4,57 +4,14 @@
     #{{ node }} : {{ ip }}
 {% endfor %}
 
-SCRIPT_NAME="rftest2"
-LXCDIR=/var/lib/lxc
-MONGODB_CONF=/etc/mongodb.conf
-RF_HOME=RouteFlow
 
+./emulator/templates/controller_functions.sh {{ mongodb_address }} {{ mongodb_port }} {{ controller_port }}
+
+cd RouteFlow
 export PATH=$PATH:/usr/local/bin:/usr/local/sbin
 export PYTHONPATH=$PYTHONPATH:$RF_HOME
 
-cd $RF_HOME
-
-wait_port_listen() {
-    port=$1
-    while ! `nc -z localhost $port` ; do
-        echo -n .
-        sleep 1
-    done
-}
-
-echo_bold() {
-    echo -e "\033[1m${1}\033[0m"
-}
-
-kill_process_tree() {
-    top=$1
-    pid=$2
-
-    children=`ps -o pid --no-headers --ppid ${pid}`
-
-    for child in $children
-    do
-        kill_process_tree 0 $child
-    done
-
-    if [ $top -eq 0 ]; then
-        kill -9 $pid &> /dev/null
-    fi
-}
-
-
-echo_bold "-> Setting up MongoDB..."
-sed -i "/bind_ip/c\bind_ip = 127.0.0.1,{{ mongodb_address }}" $MONGODB_CONF
-service mongodb restart
-wait_port_listen {{ mongodb_port }}
-
-echo_bold "-> Starting the controller and RFPRoxy..."
-cd pox
-./pox.py log.level --=INFO topology openflow.topology openflow.discovery rfproxy rfstats &
-cd -
-wait_port_listen {{ controller_port }}
-
-echo_bold "-> Creating rfconfig.csv ... "
+echo "-> Creating rfconfig.csv ... "
 
 VM_ID=0
 VM_PORT=1
@@ -74,11 +31,11 @@ echo "vm_id,vm_port,ct_id,dp_id,dp_port" > rfconfig.csv
 
 
 
-echo_bold "-> Starting RFServer..."
+echo "-> Starting RFServer..."
 ./rfserver/rfserver.py rfconfig.csv &
 
 
-echo_bold "-> Starting the control plane network (dp0 VS)..."
+echo "-> Starting the control plane network (dp0 VS)..."
 ovs-vsctl --may-exist add-br dp0
 {% for container_name, interfaces in dpinterfaces.items() %}
     {% for interface, mac in interfaces.items() %}
@@ -87,6 +44,6 @@ ovs-vsctl --may-exist add-br dp0
 {% endfor %}
 
 
-echo_bold "-> adding controller to dp0"
+echo "-> adding controller to dp0"
 ovs-vsctl set Bridge dp0 other-config:datapath-id=7266767372667673
 ovs-vsctl set-controller dp0 tcp:127.0.0.1:{{ controller_port }}
