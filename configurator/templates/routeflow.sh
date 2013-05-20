aec257a063d80659021faf5e98775628a8dfbf34
Ah-Lun Tang <tang@ahlun.be>
2013-05-20 22:58:39 +0200

    routeflow topology

--- configurator/templates/routeflow.sh
+++ configurator/templates/routeflow.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 cd RouteFlow
+
+aptitude -y install build-essential git libboost-dev libboost-program-options-dev libboost-thread-dev libboost-filesystem-dev iproute-dev openvswitch-switch mongodb python-pymongo
+
+
 ./build.sh -c ${1}
