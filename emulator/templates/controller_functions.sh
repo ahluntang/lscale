37699c3a46a05185e4f8bc8529b165a2c378507e
Ah-Lun Tang <tang@ahlun.be>
2013-05-21 00:04:58 +0200

    routeflow topology

--- emulator/templates/controller_functions.sh
+++ emulator/templates/controller_functions.sh
@@ -42,12 +42,12 @@ kill_process_tree() {
 echo_bold "-> Setting up MongoDB..."
 sed -i "/bind_ip/c\bind_ip = 127.0.0.1,${1}" $MONGODB_CONF
 service mongodb restart
-wait_port_listen ${2}
+#wait_port_listen ${2}
 
 echo_bold "-> Starting the controller and RFPRoxy..."
 cd pox
 ./pox.py log.level --=INFO topology openflow.topology openflow.discovery rfproxy rfstats &
 cd -
-wait_port_listen ${3}
+#wait_port_listen ${3}
 
-echo_bold "-> Creating rfconfig.csv ... "
+#echo_bold "-> Creating rfconfig.csv ... "
