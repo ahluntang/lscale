31dfd66dd8dd9e3caa6a296eff47a67977acbb75
Ah-Lun Tang <tang@ahlun.be>
2013-05-21 14:04:06 +0200

    routeflow topology

--- emulator/elements.py
+++ emulator/elements.py
@@ -186,7 +186,7 @@ class Container(object):
             # log into the lxc shell
             self.shell.sendline(self.username)
             self.shell.sendline(self.password)
-            self.shell.expect("{}@.*".format(self.username))
+            self.shell.expect(".*applicable.*")
             self.shell.sendline("sudo su")
             self.shell.sendline(self.password)
             logging.getLogger(__name__).info("Changed user to root.")
