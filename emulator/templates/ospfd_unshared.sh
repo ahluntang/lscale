cea7e2f568d4e278e081886fa7a1d05505c8fe12
Ah-Lun Tang <tang@ahlun.be>
2013-05-24 18:17:14 +0200

    topology: quagga

--- emulator/templates/ospfd_unshared.sh
+++ emulator/templates/ospfd_unshared.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd ~/quagga/{{ container_id }}
-/usr/lib/quagga/ospfd -f ospfd.conf --daemon -A 127.0.0.1
+/usr/lib/quagga/ospfd -f ospfd.conf -A 127.0.0.1
 
 echo "SCRIPTFINISHED"
--- generator/topology/quagga.py
+++ generator/topology/quagga.py
@@ -14,10 +14,16 @@ def debian(container):
     return ""
 
 
-def ospf(networks):
-    config = 'router ospf'
+def ospf(networks, container):
+    config = '!\nrouter ospf\n!'
     for network in networks:
         #addresses.append("network {} area 0".format(network))
         config += "\n    network {} area 0".format(network)
+    config +="\n!\n"
+    for interface in container.interfaces:
+
+        config += "interface {}\n".format(interface.interface_id)
+        config += "    ip ospf hello-interval 1\n"
+        config += "    ip ospf dead-interval 4\n"
 
     return config
