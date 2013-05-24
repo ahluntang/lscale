384c351a373667eea9c1c1d8f34f86050e2dca45
Ah-Lun Tang <tang@ahlun.be>
2013-05-24 18:59:50 +0200

    topology: quagga

--- emulator/templates/ospfd_unshared.sh
+++ emulator/templates/ospfd_unshared.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd ~/quagga/{{ container_id }}
-/usr/lib/quagga/ospfd -f ospfd.conf -A 127.0.0.1
+unshare --mount --uts --ipc /usr/lib/quagga/ospfd -f ospfd.conf -A 127.0.0.1
 
 echo "SCRIPTFINISHED"
