8efe2ec54e9b54ac259a006fec77020c9f017d13
Ah-Lun Tang <tang@ahlun.be>
2013-05-25 15:32:03 +0200

    topology: quagga

--- generator/examples/stars.py
+++ generator/examples/stars.py
@@ -51,6 +51,7 @@ def create(last_host_id, last_container_id, last_link_id, starting_address):
     gen_components.connect_containers(star1_center, star2_center, star1_component, star2_component, subnet)
 
     # connecting third star component
+    addressing_scheme = addressing.addressing_for_star_component(hosts)
     star3_component = gen_components.create_star(host, hosts, addressing_scheme, ContainerType.LXC, "quagga")
     components[star3_component.component_id] = star3_component
 
